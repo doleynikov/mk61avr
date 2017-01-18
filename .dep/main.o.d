f61f2abd77935f3e899080a6b814c8e4cc15976d
Дмитрий Олейников <doleynikov@gmail.com>
2017-01-18 11:44:51 +0300

    1

---
+++ .dep/main.o.d
@@ -0,0 +1,22 @@
+main.o: main.c config.h micro_os.h mk61types.h usart_module.h time.h \
+ i2c.h store.h terminal.h lcd.h extlib.h
+
+config.h:
+
+micro_os.h:
+
+mk61types.h:
+
+usart_module.h:
+
+time.h:
+
+i2c.h:
+
+store.h:
+
+terminal.h:
+
+lcd.h:
+
+extlib.h:
---
+++ .dep/uart.o.d
@@ -0,0 +1,9 @@
+uart.o: uart.c usart_module.h config.h micro_os.h mk61types.h
+
+usart_module.h:
+
+config.h:
+
+micro_os.h:
+
+mk61types.h:
