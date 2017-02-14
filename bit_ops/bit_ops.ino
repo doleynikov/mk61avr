a1e0d424fbf6b16b861f8ca2f723231474c1b5c3
Дмитрий Олейников <doleynikov@gmail.com>
2017-02-14 16:21:43 +0300

    bit-op : almost working keyboard

--- bit_ops/bit_ops.ino
+++ bit_ops/bit_ops.ino
@@ -1,62 +1,79 @@
 /*  (c0,c1,c2,c3) + (d2,d3,d4,d5)
- *   (b0,b1,b2,b3,b4)
- */
+     (b0,b1,b2,b3,b4)
+*/
 #define  NUM_ROWS  8
 #define  NUM_COLS  5
 
-byte scanMask=0b00000001;
-byte maskB=0b11100000;
-byte maskC=0b11110000;
-byte maskD=0b11000011;
+byte scanMask = 0b00000001;
+byte maskB = 0b11100000;
+byte maskC = 0b11110000;
+byte maskD = 0b11000011;
 
 byte rowPins[NUM_ROWS] = {A0, A1, A2, A3, 2, 3, 4, 5}; //connect to the row pinouts of the keypad
 byte colPins[NUM_COLS] = {8, 9, 10, 11, 12}; //connect to the column pinouts of the keypad
 
- void setup(){
-/*  PORTB=PORTB&maskB;
-  PORTC=PORTC&maskC;
-  PORTD=PORTD&maskD;
-  DDRB=DDRB&maskB;//выход
-  DDRC=DDRC|maskC;//вход
-  DDRD=DDRD|maskD;//вход*/
+#define KEY_ENTER 13
+#define KEY_BRK '#'
+#define SYMBOL_SHIFT 1
+#define CAPS_SHIFT 2
 
 
-  for (byte c = 0 ; c < NUM_COLS ; c++)
-  {
-    pinMode(colPins[c], OUTPUT);
- //   digitalWrite(colPins[c], HIGH);
-  }
-  for (byte r = 0 ; r < NUM_ROWS ; r++)
-  {
-    pinMode(rowPins[r], INPUT);
- //   digitalWrite(rowPins[r], HIGH);
-  }
-  
+// Keyboard
+#define  ROWS  8
+#define  COLS  5
+char a[ROWS][COLS] = {
+  {'5', '4', '3', '2', '1'},
+  {'T', 'R', 'E', 'W', 'Q'},
+  {'G', 'F', 'D', 'S', 'A'},
+  {'V', 'C', 'X', 'Z', CAPS_SHIFT},
+  {'6', '7', '8', '9', '0'},
+  {'Y', 'U', 'I', 'O', 'P'},
+  {'H', 'J', 'K', 'L', KEY_ENTER},
+  {'B', 'N', 'M', SYMBOL_SHIFT, ' '}
+};
+
+int i = 0;
+int j = 0;
+
+void setup() {
   Serial.begin(9600);
-  }
-byte curC=0;
-byte lstC=0;
-byte curD=0;
-byte lstD=0;
-
- void loop(){
-PORTB=PORTB|0b00000100; 
-delay(10);
-  curC=PINC|maskC;
-  curD=PIND|maskD;
-  if (curC!=lstC) {
-    Serial.print("  :  C:");
-    Serial.print (lstC,BIN);
-    Serial.print("  -  ");
-    Serial.println (curC,BIN);
-    }
-    lstC=curC;
-  if (curD!=lstD) {
-    Serial.print("  :  D:");
-    Serial.print (lstD,BIN);
-    Serial.print("  -  ");
-    Serial.println (curD,BIN);
-    }
-    lstD=curD;
-delay(500);
 }
+
+
+void loop() {
+  DDRB = 0xFF;
+  PORTB = 0xFF;
+
+  DDRC = 0x00;
+  PORTC = 0b00001111; //0x0F;
+  DDRD = 0x00;
+  PORTD = 0b00111100; //0x0F;
+
+
+  while (1) {
+    for (i = 0; i < COLS; i++)
+    {
+      PORTB &= ~(1 << i);
+      delay (20);
+      for (j = 0; j < ROWS/2; j++)
+      {
+        if (!(PINC & (1 << j)))
+        {
+          Serial.println(a[j][i]);
+
+        }
+        if (!(PIND & (1 << j + 2)))
+        {
+          Serial.println(a[j + 4][i]);
+
+        }
+      }
+      PORTB |= (1 << i);
+      delay(10);
+    }
+
+  }
+
+  return 0;
+}
+
